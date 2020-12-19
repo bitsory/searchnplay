e79da5796c56e49742ddd26a08493b14105183b9
bitsory <rangdad@gmail.com>
2020-12-19 00:36:38 -0800

    Updates

---
+++ precache-manifest.6bdf6f93785a57a7cbf752433b38ef7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd1c34f76b94660c62790f671611a1e",
+    "url": "/searchnplay/index.html"
+  },
+  {
+    "revision": "a080074ec98c320c92e6",
+    "url": "/searchnplay/static/css/main.b6b5506b.chunk.css"
+  },
+  {
+    "revision": "6516e47f24be9239db39",
+    "url": "/searchnplay/static/js/2.39142aa0.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/searchnplay/static/js/2.39142aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a080074ec98c320c92e6",
+    "url": "/searchnplay/static/js/main.0f2c0c1d.chunk.js"
+  },
+  {
+    "revision": "de641c99be1f0434f5d1",
+    "url": "/searchnplay/static/js/runtime-main.ecf74500.js"
+  }
+]);
